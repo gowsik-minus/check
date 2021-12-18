b6c07441bba4744f17abcc56f537f8ad5de652e3
gowsik <79508376+gowsik-minus@users.noreply.github.com>
2021-12-18 14:08:21 +0530

    initial commit

---
+++ src/main/java/com/gowsik/myapp/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.gowsik.myapp.repository;
+
+import com.gowsik.myapp.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/gowsik/myapp/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.gowsik.myapp.service.dto;
